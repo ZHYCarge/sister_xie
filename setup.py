414829836c0a0833f1ae4f45adac6b9f96cd2dd6
ZHYCarge <8144808+zhycarge@user.noreply.gitee.com>
2022-11-28 18:50:12 +0800

    renew:更新文件

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@
 from setuptools import setup
 
 setup(
-    name='sister_xie',
+    name='xyqsister',
     version='0.0.1',
     author='ZHYCarge',
     author_email='ZHYCarge@126.com',
     long_description = '给谢妹妹写的一个专属小插件',
     url='https://github.com/ZHYCarge/sister_xie',
     description=u'写给妹妹的一个小软件（确信）',
-    packages=['sister_xie'],
+    packages=['xyqsister'],
     install_requires=[],
 
 )
--- xyqsister/__init__.py
+++ xyqsister/__init__.py
@@ -5,3 +5,7 @@ def EN():
 
 def CN():
     print("XYq是智能科学与技术唯一的妹妹，谢妹妹")
+
+def all():
+    print('XYq is the only sister of intelligent science and technology, Xie sister')
+    print("XYq是智能科学与技术唯一的妹妹，谢妹妹")
